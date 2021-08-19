4ff8971eb6b82716c60ab59f478723699330ecda
manpreetgugga <manpreetsingh4501@gmail.com>
2021-08-19 15:54:40 +0530

    Documents upload

--- app/src/main/java/in/callific/androidr/MainActivity.kt
+++ app/src/main/java/in/callific/androidr/MainActivity.kt
@@ -1,11 +1,111 @@
 package `in`.callific.androidr
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.ContentUris
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android.widget.TextView
+import java.io.File
+import java.lang.Exception
+import java.net.URISyntaxException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), IOnDocumentStreamCallBacks {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.text2).setOnClickListener {
+            if (AndroidRHelper.isAndroidROrAbove()) {
+                AndroidRHelper.openDirectory(this, 123)
+            }
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode === Activity.RESULT_OK) {
+            data?.let { AndroidRHelper.fetchPath(this, it, this) }
+        }
+    }
+
+    override fun onSuccess(uri: Uri?) {
+        findViewById<TextView>(R.id.path).text = "Path Selected for PDF in android R : " + uri?.let { getPath(it) }
+    }
+
+    override fun onFail() {
+    }
+
+    @SuppressLint("NewApi")
+    @Throws(URISyntaxException::class)
+    protected fun getPath(uri: Uri): String? {
+        var uri = uri
+        var selection: String? = null
+        var selectionArgs: Array<String>? = null
+        // Uri is different in versions after KITKAT (Android 4.4), we need to
+        // deal with different Uris.
+        if (DocumentsContract.isDocumentUri(applicationContext, uri)) {
+            if (isExternalStorageDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).toTypedArray()
+                return Environment.getExternalStorageDirectory().toString() + "/" + split[1]
+            } else if (isDownloadsDocument(uri)) {
+                val id = DocumentsContract.getDocumentId(uri)
+                uri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), java.lang.Long.valueOf(id))
+            } else if (isMediaDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).toTypedArray()
+                val type = split[0]
+                if ("image" == type) {
+                    uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                } else if ("video" == type) {
+                    uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                } else if ("audio" == type) {
+                    uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                }
+                selection = "_id=?"
+                selectionArgs = arrayOf(
+                    split[1]
+                )
+            }
+        }
+        if ("content".equals(uri.scheme, ignoreCase = true)) {
+            val projection = arrayOf(
+                MediaStore.Images.Media.DATA
+            )
+            var cursor: Cursor? = null
+            try {
+                cursor = contentResolver.query(uri, projection, selection, selectionArgs, null)
+                val column_index = cursor!!.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        } else if ("file".equals(uri.scheme, ignoreCase = true)) {
+            return uri.path
+        }
+        return null
+    }
+
+    fun isExternalStorageDocument(uri: Uri): Boolean {
+        return "com.android.externalstorage.documents" == uri.authority
+    }
+
+    fun isDownloadsDocument(uri: Uri): Boolean {
+        return "com.android.providers.downloads.documents" == uri.authority
+    }
+
+    fun isMediaDocument(uri: Uri): Boolean {
+        return "com.android.providers.media.documents" == uri.authority
+    }
+
 }
